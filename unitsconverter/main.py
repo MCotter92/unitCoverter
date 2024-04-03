a88b4a7fa7a6d91db41b5abe0b694c202f0b6903
maccotter11 <122413834+maccotter11@users.noreply.github.com>
2024-04-03 18:46:42 -0400

    finished initial attempt.

--- unitsconverter/main.py
+++ unitsconverter/main.py
@@ -1,187 +1,116 @@
 import math
-
-UNITS = ["Inches", "Feet", "Centimeters", "Meters", "Yards"]
-COMBOS = []
+import pretty_errors
 
 
-def in_to_feet(inches) -> float:
-    feet = inches * 12
-    return feet
+class Convert:
+    """
+    A class to convert between some basic units of measurement.
+    """
 
+    @classmethod
+    def __init__(self, inch, ft, cm, m, yd):
+        self.inch = inch
+        self.ft = ft
+        self.cm = cm
+        self.m = m
+        self.yd = yd
 
-def in_to_cm(inches) -> float:
-    cm = inches * 2.54
-    return cm
+    @classmethod
+    def in_to_ft(self) -> float:
+        feet = self.inch / 12
+        return feet
 
+    @classmethod
+    def in_to_cm(self) -> float:
+        cent = self.inches * 2.54
+        return cent
 
-def in_to_meters(inches) -> float:
-    meters = inches / 39.37
-    return meters
+    @classmethod
+    def in_to_meters(self) -> float:
+        meters = self.inches / 39.37
+        return meters
 
+    @classmethod
+    def in_to_yards(self) -> float:
+        yards = self.inch / 36
+        return yards
 
-def in_to_yards(inches) -> float:
-    yards = inches / 36
-    return yards
+    @classmethod
+    def feet_to_inches(self) -> float:
+        inches = self.feet * 12
+        return inches
 
+    @classmethod
+    def feet_to_cm(self) -> float:
+        cent = self.feet * 30.48
+        return cent
 
-def feet_to_inches(feet) -> float:
-    inches = feet / 12
-    return inches
+    @classmethod
+    def feet_to_meters(self) -> float:
+        meters = self.feet / 3.281
+        return meters
 
+    @classmethod
+    def feet_to_yards(self) -> float:
+        yards = self.feet / 3
+        return yards
 
-def feet_to_cm(feet) -> float:
-    cm = feet * 30.48
-    return cm
+    @classmethod
+    def cm_to_in(self) -> float:
+        inches = self.cm / 2.54
+        return inches
 
+    @classmethod
+    def cm_to_feet(self) -> float:
+        feet = self.cm / 30.48
+        return feet
 
-def feet_to_meters(feet) -> float:
-    meters = feet / 3.281
-    return meters
+    @classmethod
+    def cm_to_meters(self) -> float:
+        meters = self.cm / 100
+        return meters
 
+    @classmethod
+    def cm_to_yards(self) -> float:
+        yards = self.cm / 91.44
+        return yards
 
-def feet_to_yards(feet) -> float:
-    yards = feet / 3
-    return yards
+    @classmethod
+    def meters_to_inches(self) -> float:
+        inches = self.meters * 39.37
+        return inches
 
+    @classmethod
+    def meters_to_feet(self) -> float:
+        feet = self.meters * 3.281
+        return feet
 
-def cm_to_in(cm) -> float:
-    inches = cm / 2.54
-    return inches
+    @classmethod
+    def meters_to_cm(self) -> float:
+        cm = self.meters * 100
+        return cm
 
+    @classmethod
+    def meters_to_yards(self) -> float:
+        yards = self.meters * 1.094
+        return yards
 
-def cm_to_feet(cm) -> float:
-    feet = cm / 30.48
-    return feet
+    @classmethod
+    def yards_to_inches(self) -> float:
+        inches = self.yards * 36
+        return inches
 
+    @classmethod
+    def yards_to_feet(self) -> float:
+        feet = self.yards * 3
+        return feet
 
-def cm_to_meters(cm) -> float:
-    meters = cm / 100
-    return meters
+    @classmethod
+    def yards_to_cm(self) -> float:
+        cm = self.yards * 91.44
+        return cm
 
-
-def cm_to_yards(cm) -> float:
-    yards = cm / 91.44
-    return yards
-
-
-def meters_to_inches(meters) -> float:
-    inches = meters * 39.37
-    return inches
-
-
-def meters_to_feet(meters) -> float:
-    feet = meters * 3.281
-    return feet
-
-
-def meters_to_cm(meters) -> float:
-    cm = meters * 100
-    return cm
-
-
-def meters_to_yards(meters) -> float:
-    yards = meters * 1.094
-    return yards
-
-
-def yards_to_inches(yards) -> float:
-    inches = yards * 36
-    return inches
-
-
-def yards_to_feet(yards) -> float:
-    feet = yards * 3
-    return feet
-
-
-def yards_to_cm(yards) -> float:
-    cm = yards * 91.44
-    return cm
-
-
-def yards_to_meters(yards) -> float:
-    meters = yards / 1.094
-    return meters
-
-
-def find_combos():
-    for i in UNITS:
-        for j in UNITS:
-            COMBOS.append((i, j))
-
-
-def check_start_amount(start_amount):
-    if type(start_amount) != int or type(start_amount) != float:
-        raise ValueError("Starting amount must be either a float or int.")
-
-
-def check_start_unit(start_unit):
-    if type(start_unit) != str:
-        raise ValueError("Starting unit must be a string.")
-
-
-def check_end_unit(end_unit):
-    if type(end_unit) != str:
-        raise ValueError("Starting unit must be a string.")
-
-
-def conversion(start_amount, start_unit, end_unit):
-    units = (start_unit, end_unit)
-    match units:
-        # Inches
-        case ("Inches", "Feet"):
-            in_to_feet(start_amount)
-        case ("Inches", "Centimeters"):
-            in_to_cm(start_amount)
-        case ("Inches", "Meters"):
-            in_to_meters(start_amount)
-        case ("Inches", "Yards"):
-            in_to_yards(start_amount)
-        # Feet
-        case ("Feet", "Inches"):
-            feet_to_inches(start_amount)
-        case ("Feet", "Centimeters"):
-            feet_to_cm(start_amount)
-        case ("Feet", "Yards"):
-            feet_to_yards(start_amount)
-        case ("Feet", "Meters"):
-            feet_to_meters(start_amount)
-        # Yards
-        case ("Yards", "Centimeters"):
-            yards_to_cm(start_amount)
-        case ("Yards", "Feet"):
-            yards_to_feet(start_amount)
-        case ("Yards", "Inches"):
-            yards_to_inches(start_amount)
-        case ("Yards", "Meters"):
-            feet_to_cm(start_amount)
-        # Meters
-        case ("Meters", "Feet"):
-            meters_to_cm(start_amount)
-        case ("Meters", "Inches"):
-            meters_to_inches
-        case ("Meters", "Yards"):
-            meters_to_yards
-        case ("Meters", "Centimeters"):
-            meters_to_cm(start_amount)
-
-
-def main():
-
-    start_amount = input("Please enter a starting amount as either a float or an int: ")
-    check_start_amount(start_amount)
-
-    start_unit = input("Please input a starting unit of measurement: ")
-    check_start_unit(start_unit)
-
-    end_unit = input("Please enter the desired unit of measurement to convert to: ")
-    check_end_unit(end_unit)
-
-    return conversion(start_amount, start_unit, end_unit)
-
-
-if __name__ == "__main__":
-    main()
-
-
-print(conversion(int(12), "Inches", "Feet"))
+    @classmethod
+    def yards_to_meters(self) -> float:
+        meters = self.yards / 1.094
+        return meters
